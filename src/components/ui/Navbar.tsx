124792ae029862906aa6b416b4f49104bdb75997
Ajay Negi <ajaynegi3345@icloud.com>
2024-09-01 18:28:11 +0530

    format code

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,39 +1,18 @@
-// import Link from "next/link";
-// import styles from "./navbar.module.css";
-//
-// export default function Navbar() {
-//     return (<>
-//             <div className={styles["navbar-container"]}>
-//                 <nav className={styles["navbar"]}>
-//                     <Link href="/" className={styles["heading"]}>
-//                         <h3 className={styles["title"]}>UTTARAKHAND CULTURE</h3>
-//                     </Link>
-//                     <Link href="/" className={styles.link}>LOGIN</Link>
-//                 </nav>
-//             </div>
-//             <div className={styles["fade-navbar-effect"]}></div>
-//             <div className={styles["empty-navbar"]}></div>
-//         </>
-//     );
-// }
 import Link from "next/link";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
-    return (
-        <>
-            <div className={styles.navbarContainer}>
-                <nav className={styles.navbar}>
-                    <Link href="/" className={styles.heading}>
-                        <h3 className={styles.title}>UTTARAKHAND CULTURE</h3>
+    return (<>
+            <div className={styles["navbar-container"]}>
+                <nav className={styles["navbar"]}>
+                    <Link href="/" className={styles["heading"]}>
+                        <h3 className={styles["title"]}>UTTARAKHAND CULTURE</h3>
                     </Link>
-                    <Link href="/" className={styles.link}>
-                        LOGIN
-                    </Link>
+                    <Link href="/" className={styles.link}>LOGIN</Link>
                 </nav>
             </div>
-            <div className={styles.fadeNavbarEffect}/>
-            <div className={styles.emptyNavbar}/>
+            <div className={styles["fade-navbar-effect"]}></div>
+            <div className={styles["empty-navbar"]}></div>
         </>
-);
+    );
 }
