7ddc6d524fc70632f5e83cdc6e2b1d4c589c2976
Ajay Negi <ajaynegi3345@icloud.com>
2024-09-23 00:47:28 +0530

    Update

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,10 +1,8 @@
 import styles from "./page.module.css"
-import Link from "next/link";
-import Card from "@/components/ui/Card";
 import SectionCard from "@/components/ui/SectionCard";
 import Image from "next/image";
-import GheeSankranti from "/public/Ghee_Sankranti.jpeg"
-import Mountain from '/public/mountain.svg'
+import Mountain from '/public/mountain.svg';
+import Bagwal from "/public/bagwal.jpg";
 
 export default function Explore() {
     return (
@@ -18,12 +16,12 @@ export default function Explore() {
             <SectionCard cardTitle={"TREKKING IN UTTARAKHAND"} title={"EXPLORE TREKKING"}
                          subTitle={"FIND OLD TREKKING ROUTES, SEARCH PLACES TO SUIT YOUR NEEDS"}
                          description={"Trekking in Uttarakhand offers an exhilarating experience through some of the most breathtaking landscapes in the Himalayas. Known as the 'Land of the Gods,' Uttarakhand is home to a diverse range of trekking trails that cater to both beginners and seasoned trekkers. From the popular Valley of Flowers, known for its vibrant alpine flora, to the challenging Roopkund trek, famous for its mysterious skeletal lake, the state provides a perfect blend of adventure and natural beauty. Trekkers can explore pristine forests, towering snow-capped peaks, crystal-clear rivers, and ancient temples along the way. The spiritual significance of many trails, such as the Kedarnath and Hemkund Sahib treks, adds a deeper dimension to the journey, attracting not only nature enthusiasts but also pilgrims. Whether it's the awe-inspiring beauty of the Nanda Devi National Park or the serene paths of Chopta, trekking in Uttarakhand offers a chance to connect with nature while discovering the cultural richness of the region's remote villages. With its well-marked trails and stunning vistas at every turn, Uttarakhand remains a paradise for trekkers seeking adventure, tranquility, and a deep connection to the Himalayas."}
-                         image={""} readMoreLink={"/trekking"}/>
+                         image={Bagwal} readMoreLink={"/trekking"}/>
 
             <SectionCard cardTitle={"UTTARAKHAND LANGUAGE"} title={"EXPLORE LANGUAGE"}
                          subTitle={"KUMAONI · GARHWALI · JAUNSARI"}
                          description={"Uttarakhand is a state rich in linguistic diversity, with its residents speaking several regional languages and dialects. The most widely spoken languages include Kumaoni, Garhwali, and Jaunsari, which vary based on the region's geography and communities. Each of these languages carries a deep cultural significance, passed down through generations, reflecting the local folklore, traditions, and lifestyle. These languages serve as a vital link to the past, preserving Uttarakhand’s heritage through oral literature, songs, and storytelling. By learning and promoting these languages, we continue to celebrate and preserve the unique identity of this beautiful region."}
-                         image={""} readMoreLink={"/language"}/>
+                         image={Bagwal} readMoreLink={"/language"}/>
 
             <section>
 
--- src/components/ui/SectionCard.tsx
+++ src/components/ui/SectionCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "@/components/ui/sectionCard.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import {StaticImageData} from 'next/image';
 import GheeSankranti from "/public/Ghee_Sankranti.jpeg"
 
 type SectionCardProps = {
@@ -9,36 +10,50 @@ type SectionCardProps = {
     title: string;
     subTitle: string;
     description: string;
-    image: string;
+    image: StaticImageData;
     readMoreLink: string
+    location?: string
 }
 
 
 const SectionCard = ( props : SectionCardProps) => {
     return (
-            <section className={styles.sectionCardContainer}>
+        <section className={styles.sectionCardContainer}>
 
-                <div className={styles.sectionCardTitle}>
-                    <div className={styles.sectionCardTitleLine}></div>
-                    <p>{props.cardTitle}</p></div>
+            <div className={styles.sectionCardTitle}>
+                <div className={styles.sectionCardTitleLine}></div>
+                <p>{props.cardTitle}</p>
+            </div>
 
 
-                <div className={styles.sectionCard}>
+            <div className={styles.sectionCard}>
                 <div className={styles.sectionCardLeftSide}>
-                        <h2 className={styles.title}>{props.title}</h2>
-                        <p className={styles.subTitle}>{props.subTitle}</p>
-                        <p className={styles.description}>{props.description}</p>
-                        <div className={styles.linkContainer}>
-                            <div className={styles.linkContainer1}><Link href={props.readMoreLink ? props.readMoreLink : '#'}>READ MORE</Link></div>
-                        </div>
-                    </div>
+                    <h2 className={styles.title}>{props.title}</h2>
+                    <p className={styles.subTitle}>{props.subTitle}</p>
+                    <p className={styles.description}>{props.description}</p>
 
-                    <div className={styles.imageContainer}>
-                        <Image src={GheeSankranti} alt={""} width={300} height={200} placeholder={"blur"}/>
+                    <div className={styles.linkContainer}>
+                        <div className={styles.linkContainer1}><Link href={props.readMoreLink ? props.readMoreLink : '#'}>READ MORE</Link></div>
+                        {props.location && (
+                            <div className={styles.linkContainer2}>
+                                <Link
+                                    href={props.location}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    VIEW LOCATION
+                                </Link>
+                            </div>
+                        )}
                     </div>
-
                 </div>
-            </section>
+
+                <div className={styles.imageContainer}>
+                    <Image src={props.image} alt={""} width={300} height={200} placeholder={"blur"}/>
+                </div>
+
+            </div>
+        </section>
     );
 };
 
