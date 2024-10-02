69fcb15bfad60b4530ae0e18e2696d04a9bec526
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-02 12:10:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,6 +62,8 @@ export default function Home() {
                 </div>
             </header>
 
+                {/*<Screen/>*/}
+
             <section className={styles["home-story-container"]}>
                 <div className={styles["why-built-it-container"]}>
                     <div className={styles["built-image-container"]}>
@@ -81,10 +83,16 @@ export default function Home() {
                     <div className={styles["built-text-container"]}>
                         <p className={styles["why-built-it-title"]}>WHY WE BUILD IT</p>
                         <p className={styles["built-description-container"]}>
-                            A concerning trend is emerging in Uttarakhand, where the younger generation is gradually
+                        A concerning trend is emerging in
+                            Uttarakhand, where the younger generation is gradually
                             forgetting their cultural heritage and traditional rituals. This decline in cultural
                             knowledge is evident across generations, with each successive generation possessing less
-                            knowledge than the previous one...
+                            knowledge than the previous one. For instance, the amount of cultural knowledge possessed by
+                            grandparents is significantly more than that of their children, and the cultural knowledge
+                            of today&apos;s youth is likely to be even less than that of their parents. If this trend
+                            continues, there is a risk that the cultural heritage of Uttarakhand may eventually
+                            disappear from the society. This loss would not only erase the region&apos;s rich cultural
+                            identity but also deprive future generations of their cultural roots and traditions.
                         </p>
                     </div>
                 </div>
@@ -95,7 +103,13 @@ export default function Home() {
                         <div className={styles["journey-left-container"]}>
                             <h3>EXPLORE UTTARAKAND</h3>
                             <p className={styles["built-description-container"]}>
-                                To address this problem, we have come up with a solution to digitize this cultural knowledge...
+                            To address this problem, we have
+                                come up with a solution to digitize this cultural
+                                knowledge. Since today&apos;s generation is more comfortable with technology, we aim to
+                                provide a platform where they can learn about their cultural heritage using their
+                                devices, from anywhere in the world. By doing so, we hope to make cultural knowledge
+                                more accessible and engaging for the younger generation, and thus help preserve the rich
+                                cultural heritage of Uttarakhand
                             </p>
                             <Link href={"/explore"}>Start Your Journey</Link>
                         </div>
@@ -122,7 +136,7 @@ export default function Home() {
                 cardTitle={"UPCOMING FESTIVAL"}
                 title={"Egaas Bagwal"}
                 subTitle={"12 NOVEMBER · UTTARAKHAND"}
-                description={"Egaas Bagwal is a unique festival celebrated 11 days after Diwali..."}
+                description={"Egaas Bagwal is a unique festival celebrated 11 days after Diwali in the hilly regions of Uttarakhand. According to local belief, Lord Rama returned from exile late to these areas, which is why people here celebrate Egaas with great enthusiasm. The festival involves preparing traditional delicacies, performing joyful folk dances, and lighting up homes, much like Diwali. A standout tradition is spinning a flaming rope called \"Bhailo\", where villagers twirl a fire-lit rope, creating a mesmerizing display symbolizing light’s triumph over darkness. Egaas Bagwal reflects Uttarakhand’s rich cultural heritage, offering a glimpse into the community’s deep-rooted traditions and festive spirit. For the people of Uttarakhand, it’s not just a festival, but a meaningful tribute to their ancestors and the vibrant life of the hills."}
                 image={GheeSankranti}
                 readMoreLink={""}
                 location={""}
@@ -133,7 +147,7 @@ export default function Home() {
                 cardTitle={"UPCOMING FAIR"}
                 title={"Nanda Devi Mahotsav"}
                 subTitle={"8 SEPTEMBER - 19 SEPTEMBER · NAINITAL"}
-                description={"The Nanda Devi Mahotsav is a grand celebration dedicated to Goddess Nanda Devi..."}
+                description={"The Nanda Devi Mahotsav is a grand celebration dedicated to Goddess Nanda Devi, the revered deity of the Kumaon region. This vibrant festival, which takes place annually in Nainital, is deeply ingrained in the cultural fabric of Uttarakhand. It is believed to have started centuries ago to honor Nanda Devi, who is considered the protector of the region. The festival is marked by colorful processions, traditional dance performances, and rituals that attract devotees and tourists alike. The highlight of the event is the ‘Nanda Jaat Yatra,’ where a grand procession of devotees carries a palanquin of the goddess through the scenic landscapes of Uttarakhand, symbolizing her journey to her maternal home"}
                 image={Nanda_Sunanda}
                 readMoreLink={""}
                 location={"https://www.google.com/maps/place/Maa+Nanda+Devi+Temple,+Almora/@29.5998843,79.6584971,17z/data=!3m1!4b1!4m6!3m5!1s0x39a0b76914898c8b:0x887deeed7e5d3c82!8m2!3d29.5998843!4d79.661072!16s%2Fg%2F11fmrk8bnv?hl=en&entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D"}
