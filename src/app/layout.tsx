17a52d89941515ebb712e131eef80e271a2b15ba
Ajay Negi <97536703+ajaynegi45@users.noreply.github.com>
2024-10-03 00:49:16 +0530

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import localFont from "next/font/local";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
-// import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const customFont = localFont({
     src: "../font/customFont.otf",
@@ -18,11 +18,11 @@ export const metadata: Metadata = {
     openGraph: {
         title: "Uttarakhand Culture",
         description: "Explore the rich cultural heritage of Uttarakhand.",
-        url: "https://uttarakhand-culture.vercel.app", 
+        url: "https://uttarakhand-culture.vercel.app",
         siteName: "Uttarakhand Culture",
         images: [
             {
-                url: "https://uttarakhand-culture.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FheroMapImage.fc8fad10.webp&w=828&q=75", 
+                url: "https://uttarakhand-culture.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FheroMapImage.fc8fad10.webp&w=828&q=75",
                 width: 800,
                 height: 600,
                 alt: "Cultural Heritage of Uttarakhand",
