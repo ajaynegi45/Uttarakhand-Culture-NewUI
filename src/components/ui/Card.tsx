0d3d21f2a776ec74e9b62eff4e53124d53902f4c
suvam <suvampravu@gmail.com>
2024-10-04 05:25:26 +0530

    fixed it to dynamic img url accepts from props

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,32 +1,41 @@
 import Image from "next/image";
 import styles from "./Card.module.css";
 import Link from "next/link";
-import Roopkund from "/public/roopkund.jpg"
-
 
 type CardProps = {
-    title: string;
-    subTitle: string;
-    description: string;
-    image: string;
-    readMoreLink?: string
-}
+  title: string;
+  subTitle: string;
+  description: string;
+  image: string; 
+  readMoreLink?: string;
+};
 
 export default function Card(props: CardProps) {
-    return(
-        <section className={styles["card"]}>
-            <div className={styles[""]} >
-                <h2 className={styles["title"]}>{props.title}</h2>
-                <p className={styles["subTitle"]}>{props.subTitle}</p>
-                <p className={styles["description"]}>{props.description}</p>
-                <div className={styles["link-container"]}>
-                    <div className={styles["link-container1"]} ><Link href={"#"}>READ MORE</Link></div>
-                    <div className={styles["link-container2"]}><Link href={props.readMoreLink ? props.readMoreLink : '#'}>VIEW LOCATION</Link></div>
-                </div>
-            </div>
-            <div className={styles["image-container"]} >
-                <Image src={Roopkund} alt={""} width={300} height={200} placeholder={"blur"} />
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles["card"]}>
+      <div className={styles["text-container"]}>
+        <h2 className={styles["title"]}>{props.title}</h2>
+        <p className={styles["subTitle"]}>{props.subTitle}</p>
+        <p className={styles["description"]}>{props.description}</p>
+        <div className={styles["link-container"]}>
+          <div className={styles["link-container1"]}>
+            <Link href={"#"}>READ MORE</Link>
+          </div>
+          <div className={styles["link-container2"]}>
+            <Link href={props.readMoreLink ? props.readMoreLink : "#"}>
+              VIEW LOCATION
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles["image-container"]}>
+        <Image
+          src={props.image} // Dynamic image from props
+          alt={props.title}
+          width={500}
+          height={300}
+        />
+      </div>
+    </section>
+  );
 }
