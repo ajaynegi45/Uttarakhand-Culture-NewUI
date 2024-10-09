e5caa5151cbb9a1eb997d81e8cfe0d04214a3771
Ajay Negi <97536703+ajaynegi45@users.noreply.github.com>
2024-10-10 00:21:25 +0530

    Update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,43 +1,41 @@
 "use client";
-import { useSession, signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import {useSession, signOut} from "next-auth/react";
+import {useRouter} from "next/navigation";
 import styles from "./profile.module.css";
+import Image from "next/image";
 
 export default function Profile() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
+    const {data: session, status} = useSession();
+    const router = useRouter();
 
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
+    if (status === "loading") {
+        return <p>Loading...</p>;
+    }
 
-  if (status === "unauthenticated") {
-    router.push("/"); // Redirect to login if unauthenticated
-    return null;
-  }
+    if (status === "unauthenticated") {
+        router.push("/"); // Redirect to login if unauthenticated
+        return null;
+    }
 
-  return (
-    <div className={styles["profile-container"]}>
-      <h1 className={styles["profile-heading"]}>User Profile</h1>
-      <div className={styles["profile-details"]}>
-        <img
-          src={session?.user?.image || "/default-avatar.png"}
-          alt="User Avatar"
-          className={styles["profile-avatar"]}
-        />
-        <p>
-          <strong>Name:</strong> {session?.user?.name || "N/A"}
-        </p>
-        <p>
-          <strong>Email:</strong> {session?.user?.email || "N/A"}
-        </p>
-      </div>
-      <button
-        onClick={() => signOut({ redirect: false })}
-        className={styles["logout-button"]}
-      >
-        Logout
-      </button>
-    </div>
-  );
+    return (
+        <div className={styles["profile-container"]}>
+            <h1 className={styles["profile-heading"]}>User Profile</h1>
+            <div className={styles["profile-details"]}>
+                <Image src={session?.user?.image || "/default-avatar.png"} alt="User Avatar"
+                       className={styles["profile-avatar"]}/>
+                <p>
+                    <strong>Name:</strong> {session?.user?.name || "N/A"}
+                </p>
+                <p>
+                    <strong>Email:</strong> {session?.user?.email || "N/A"}
+                </p>
+            </div>
+            <button
+                onClick={() => signOut({redirect: false})}
+                className={styles["logout-button"]}
+            >
+                Logout
+            </button>
+        </div>
+    );
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,44 +1,44 @@
 "use client";
 import Link from "next/link";
 import styles from "./navbar.module.css";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import Image from "next/image";
 
 export default function Navbar() {
-  const session = useSession();
-  const router = useRouter();
+    const session = useSession();
+    const router = useRouter();
 
-  const handleAvatarClick = () => {
-    router.push("/profile");
-  };
-  return (
-    <>
-      <div className={styles["navbar-container"]}>
-        <nav className={styles["navbar"]}>
-          <Link href="/" className={styles["heading"]}>
-            <h3 className={styles["title"]}>UTTARAKHAND CULTURE</h3>
-          </Link>
-          {session.status == "authenticated" ? (
-            <div
-              className={styles["avatar-container"]}
-              onClick={handleAvatarClick}
-              style={{ cursor: "pointer" }}
-            >
-              <img
-                src={session.data?.user?.image || "/default-avatar.png"}
-                alt="User Avatar"
-                className={styles["avatar"]}
-              />
+    const handleAvatarClick = () => {
+        router.push("/profile");
+    };
+    return (
+        <>
+            <div className={styles["navbar-container"]}>
+                <nav className={styles["navbar"]}>
+                    <Link href="/" className={styles["heading"]}>
+                        <h3 className={styles["title"]}>UTTARAKHAND CULTURE</h3>
+                    </Link>
+                    {session.status == "authenticated" ? (
+                        <div
+                            className={styles["avatar-container"]}
+                            onClick={handleAvatarClick}
+                            style={{cursor: "pointer"}}
+                        >
+                            <Image src={session.data?.user?.image || "/default-avatar.png"}
+                                   alt="User Avatar"
+                                   className={styles["avatar"]}/>
+
+                        </div>
+                    ) : (
+                        <Link href="/auth" className={styles.link}>
+                            LOGIN
+                        </Link>
+                    )}
+                </nav>
             </div>
-          ) : (
-            <Link href="/auth" className={styles.link}>
-              LOGIN
-            </Link>
-          )}
-        </nav>
-      </div>
-      <div className={styles["fade-navbar-effect"]}></div>
-      <div className={styles["empty-navbar"]}></div>
-    </>
-  );
+            <div className={styles["fade-navbar-effect"]}></div>
+            <div className={styles["empty-navbar"]}></div>
+        </>
+    );
 }
