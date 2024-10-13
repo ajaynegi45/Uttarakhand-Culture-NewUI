56e58ae2f4e719f6368ec80b9f6c1fc0288628e7
suvam <suvampravu@gmail.com>
2024-10-14 02:03:34 +0530

    remove addtional dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -2,38 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "uttarakhandhaat.com",
-        pathname: "/wp-content/uploads/**",
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.istockphoto.com',
-        pathname: '/id/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.dsource.in',
-        pathname: '/sites/default/files/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.shutterstock.com',
-        pathname: '/image-photo/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'c1.wallpaperflare.com',
-        pathname: '/preview/992/229/146/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
